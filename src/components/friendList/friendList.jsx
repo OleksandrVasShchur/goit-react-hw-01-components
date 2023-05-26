2881ba9ac12dd7f91a160a86db3251887897568c
Oleksandr Shchur <oleksandrschur7@gmail.com>
2023-05-26 22:32:59 +0300

    bugfix

--- src/components/friendList/friendList.jsx
+++ src/components/friendList/friendList.jsx
@@ -3,7 +3,7 @@ import FriendListItem from 'components/FriendListItem/friendListItem';
 import css from './friendList.module.css';
 
 function FriendList({ friends }) {
-    
+   
   return (
     <ul className={css.friendlist}>
       {friends.map(({id, avatar, name, isOnline}) => (
@@ -20,6 +20,13 @@ function FriendList({ friends }) {
 }
 
 FriendList.propTypes = {
-friends: PropTypes.array
+friends: PropTypes.arrayOf(
+  PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    avatar: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    isOnline: PropTypes.bool.isRequired,
+  })
+)
 }
 export default FriendList;
